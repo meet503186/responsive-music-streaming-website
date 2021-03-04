1b1623b283b60d295225ba2a5ae3e827f696f4fb
Harmeet <meet503186@gmail.com>
2021-03-04 10:04:37 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,44 @@
+var song_list = {
+    "01": {
+        title: "Love Charger",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "6:13",
+        track: "love charger.mp3"
+    },
+    "02": {
+        title: "Tu chahiye",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "6:44",
+        track: "bus tu chaiye.mp3"
+    },
+    "03": {
+        title: "Dhadkan Bhi Tum",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "6:09",
+        track: "dhadkan bhi tum.mp3"
+    },
+    "04": {
+        title: "Dhadkan Se Bapu Maari",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "5:49",
+        track: "dhadkan se bapu.mp3"
+    },
+    "05": {
+        title: "Highway",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "5:18",
+        track: "highway.mp3"
+    },
+    "06": {
+        title: "Katta tingarhda",
+        artist: "MSG",
+        genre: "Cinematic",
+        duration: "7:40",
+        track: "kata tingarhda.mp3"
+    }
+}
